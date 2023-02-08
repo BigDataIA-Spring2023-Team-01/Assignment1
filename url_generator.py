bb9c0df6430276f9168c177748f9ab3af6173315
thejas98 <thejasbharadwaj98@gmail.com>
2023-02-07 23:58:08 -0500

    Created url_generator
    Created the url_generator function- now we need to add validator and pytest function for these

---
+++ url_generator.py
@@ -0,0 +1,24 @@
+def url_gen_goes(input):
+    arr = input.split("_")
+    tproduct_code = arr[1].split("-")
+    s1 = tproduct_code[2]
+    finalProductCode =tproduct_code[0]+"-"+tproduct_code[1]+"-"+ ''.join([i for i in s1 if not i.isdigit()])
+    date = arr[3]
+    year, day_of_year, hour = date[1:5], date[5:8], date[8:10]
+    fs = "https://noaa-goes18.s3.amazonaws.com/{}/{}/{}/{}/{}".format(finalProductCode,year,day_of_year,hour,input)
+    print(fs)
+
+
+def url_gen_nexrad(input):
+    arr = input.split("_")[0]
+    year, day, hour, station = arr[4:8], arr[8:10], arr[10:12], arr[0:4]
+    fs = "https://noaa-nexrad-level2.s3.amazonaws.com/{}/{}/{}/{}/{}".format(year,day,hour,station,input)
+    return fs
+
+
+input_string = "KBYX20150804_000940_V06.gz"
+
+test = "OR_ABI-L2-DSRC-M6_G18_s20223180501179_e20223180503552_c20223180508262.nc"
+url_gen_goes(test)
+#url_gen(test)
+
