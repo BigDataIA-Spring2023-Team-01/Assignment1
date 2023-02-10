ab8f96f093770d8cdf556af488b080e370a43d3a
Anandita <deb.ana@northeastern.edu>
2023-02-09 20:46:28 -0500

    Streamlit - GEOS and Nexrad

---
+++ pages/geos.py
@@ -0,0 +1,157 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime, date
+import os
+import boto3
+import logging
+# from dotenv import load_dotenv
+import io
+import requests
+from bs4 import BeautifulSoup
+import time
+# from IPython.core.display import display, HTML
+
+#AWS KEYS 
+AWS_ACCESS_KEY = 'AKIASHB6SWSVFEFZXW2Q'
+AWS_SECRET_KEY = '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'
+USER_BUCKET_NAME = 'team01'
+region_name='us-east-1'
+ 
+s3client = boto3.client('s3',
+                        region_name='us-east-1',
+                        aws_access_key_id = 'AKIASHB6SWSVFEFZXW2Q', 
+                        aws_secret_access_key = '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'
+                        )
+
+col1, col2 = st.columns(2)
+
+with col1:
+    #Selecting station 
+    station_geos = st.text_input(
+        'Please select the station', placeholder= 'GEOS 18')
+
+    #Selecting Year
+    year_geos = st.selectbox(
+        'Please select the year',
+        ('2022', '2023'))
+
+
+    #Day of Year
+    day_of_year_geos = st.text_input('Day of Year ', max_chars=3, type='default', 
+                                    placeholder='Please enter Date of Year in DDD format ')
+
+    #Hour of Day
+    hour_of_day = st.text_input('Enter Hour of Day', max_chars=2, placeholder='Enter Hour in 24H format')
+
+    bucket = 'noaa-goes18'
+    prefix = 'ABI-L1b-RadC/{}/{}/{}/'.format(year_geos,day_of_year_geos,hour_of_day)
+
+
+    #Filename selector 
+    def list_files_as_dropdown(bucket_name, prefix):
+        try:
+            result = s3client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter ='/')
+            object_list = [x["Key"].split("/")[-1] for x in result["Contents"]]
+            selected_object = st.selectbox("Select file for download:", object_list)
+            return selected_object
+        except Exception as e:
+            st.write("An error occurred:", e)
+            return None
+
+    selected_file = list_files_as_dropdown(bucket, prefix)
+
+    if selected_file:
+        st.write("You selected:", selected_file)
+    else:
+        st.write("No objects found")
+
+    # https://noaa-goes18.s3.amazonaws.com/index.html#ABI-L1b-RadC/2023/001/00/
+    final_url = 'https://{}.s3.amazonaws.com/index.html#ABI-L1b-RadC/{}/{}/{}/{}'.format(bucket,year_geos,day_of_year_geos,hour_of_day,selected_file)
+    name_of_file = selected_file
+
+    # check_file_exists
+    def check_file_exists(filename, bucket_name):
+        try:
+            s3client.head_object(Bucket=bucket_name, Key=filename)
+            return True
+        except Exception as e:
+            return False
+
+
+    def get_file_url(year, day_of_year, hour):
+        # Base URL of the GEOS website
+        base_url = "https://noaa-goes18.s3.amazonaws.com/index.html#ABI-L1b-RadC/"
+        
+        # Year, day of year, and hour are formatted as strings with leading zeros
+        year = str(year).zfill(4)
+        day_of_year = str(day_of_year).zfill(3)
+        hour = str(hour).zfill(2)
+        
+        # Combine the base URL with the user inputs to get the file URL
+        file_url = base_url + f"{year}/{day_of_year}/{hour}"
+        
+        st.write("Link to file on GEOS website ",file_url) 
+
+
+    #Transfer file to S3 bucket
+    def transfer_file_to_S3():
+        try:
+            
+            st.write("Uploading the file to S3 bucket for download...")
+            with open(selected_file, "wb") as data:
+                data.write(requests.get(final_url).content)
+                s3client.upload_file(selected_file, USER_BUCKET_NAME, name_of_file)
+                with st.spinner('Wait for it...'):
+                    time.sleep(7)
+                    st.write("File uploaded successfully")
+            st.write('Click to download from S3 bucket', 'https://{}.s3.amazonaws.com/{}'.format(USER_BUCKET_NAME,name_of_file))
+        except Exception as e:
+            st.write("An error occurred:", str(e))
+
+    #Transfering selected file to S3 bucket 
+    if st.button('Submit'):
+        with st.spinner('Retrieving details for the file you selected, wait for it....!'):
+            time.sleep(5)
+            if check_file_exists(name_of_file, USER_BUCKET_NAME):
+                st.write(f"The file {name_of_file} already exists in the S3: {USER_BUCKET_NAME} bucket.")
+                st.write('Click to download from S3 bucket', 'https://{}.s3.amazonaws.com/{}'.format(USER_BUCKET_NAME,name_of_file))
+            else:
+                st.write(f"The file {name_of_file} does not exist in the S3: {USER_BUCKET_NAME} bucket.")
+                transfer_file_to_S3()
+                get_file_url(year_geos,day_of_year_geos,hour_of_day)
+
+with col2:
+
+    # filename_entered = st.text_input("Enter filename for download", placeholder='OR_ABI-L1b-RadC-M6C01_G18_s20230212001171_e20230212003548_c20230212003594.nc')
+    
+    def generate_url_from_filename():
+        # Get the filename entered by the user
+        filename = st.text_input("Enter the filename:")
+        if filename:
+            if st.button("Go to website"):
+                with st.spinner('Fetching link to GEOS bucket...'):
+                    time.sleep(3)
+                    # Split the filename into parts
+                    parts = filename.split("_")
+                    
+                    # Get the year, day of year, and hour from the filename
+                    year = parts[5][1:5]
+                    day_of_year = parts[5][5:8]
+                    hour = parts[5][8:10]
+                    
+                    # Base URL of the GEOS website
+                    base_url = "https://noaa-goes18.s3.amazonaws.com/index.html#ABI-L1b-RadC/"
+                    
+                    # Combine the base URL with the year, day of year, and hour to get the URL
+                    file_url = base_url + f"{year}/{day_of_year}/{hour}/"
+                    
+                    # Display the URL
+                    st.write("Access link:", file_url)
+
+    generate_url_from_filename()
+
+
+
+
+
---
+++ pages/nexrad.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime, date
+import os
+import boto3
+import logging
+# from dotenv import load_dotenv
+import io
+import requests
+from bs4 import BeautifulSoup
+import time
+# from IPython.core.display import display, HTML
+
+#AWS KEYS 
+AWS_ACCESS_KEY = 'AKIASHB6SWSVFEFZXW2Q'
+AWS_SECRET_KEY = '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'
+USER_BUCKET_NAME = 'team01'
+region_name='us-east-1'
+ 
+s3client = boto3.client('s3',
+                        region_name='us-east-1',
+                        aws_access_key_id = 'AKIASHB6SWSVFEFZXW2Q', 
+                        aws_secret_access_key = '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'
+                        )
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("Download file through field selection:")
+    #Selecting Year
+    year_nexrad = st.selectbox(
+        'Please select the year',
+        ('2022', '2023'))
+
+    #Month of Year
+    month_of_year_nexrad = st.text_input('Month of Year ', max_chars=2, type='default', 
+                                    placeholder='Please enter Month of Year in MM format')
+
+    #Day of Month
+    day_of_month_nexrad = st.text_input('Enter Day of Month', max_chars=2, placeholder='Enter Date of Month in DD format')
+
+
+    bucket = 'noaa-nexrad-level2'
+    prefix = '{}/{}/{}/'.format(year_nexrad,month_of_year_nexrad,day_of_month_nexrad)
+
+    #Station code selector 
+    def display_stationcode(bucket_name,prefix):
+        try:
+            result = s3client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter ='/')
+            object_list = [x.get("Prefix").split("/")[-2] for x in result.get("CommonPrefixes")]
+            selected_object = st.selectbox("Select station code:", object_list)
+            return selected_object
+        except Exception as e:
+            st.write("An error occurred:", e)
+            return None
+
+    selected_stationcode = display_stationcode(bucket, prefix)
+
+    if display_stationcode:
+        st.write("You selected:", selected_stationcode)
+    else:
+        st.write("No objects found")
+
+    #MADE CHANGES TO BELOW FUNCTION - ADDED PREFIX_FILE 
+    prefix_file = '{}/{}/{}/{}/'.format(year_nexrad,month_of_year_nexrad,day_of_month_nexrad,selected_stationcode)
+    #Filename selector 
+    def list_files_as_dropdown(bucket_name, prefix):
+        try:
+            result = s3client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter ='/')
+            object_list = [x["Key"].split("/")[-1] for x in result["Contents"]]
+            selected_object = st.selectbox("Select file for download:", object_list)
+            return selected_object
+        except Exception as e:
+            st.write("An error occurred:", e)
+            return None
+
+    selected_file = list_files_as_dropdown(bucket, prefix_file)
+
+    if selected_file:
+        st.write("You selected:", selected_file)
+    else:
+        st.write("No objects found")
+
+    # https://noaa-nexrad-level2.s3.amazonaws.com/index.html#2023/02/01/DOP1/
+    #URL of file to be uploaded
+
+    final_url = 'https://{}.s3.amazonaws.com/index.html#{}/{}/{}/{}/{}'.format(bucket,year_nexrad,month_of_year_nexrad,day_of_month_nexrad,selected_stationcode,selected_file)
+    name_of_file = selected_file
+
+    # check_file_exists
+    def check_file_exists(filename, bucket_name):
+        try:
+            s3client.head_object(Bucket=bucket_name, Key=filename)
+            return True
+        except Exception as e:
+            return False
+
+    #Transfer file to S3 bucket
+    def transfer_file_to_S3():
+        try:
+            st.write("Uploading the file to S3 bucket for download...")
+            with open(selected_file, "wb") as data:
+                data.write(requests.get(final_url).content)
+                s3client.upload_file(selected_file, USER_BUCKET_NAME, name_of_file)
+                with st.spinner('Almost there...'):
+                    time.sleep(5)
+                    st.write("File uploaded successfully")
+            st.write('Click to download from S3 bucket', 'https://{}.s3.amazonaws.com/{}'.format(USER_BUCKET_NAME,name_of_file))
+        except Exception as e:
+            st.write("An error occurred:", str(e))
+
+    #Transfering selected file to S3 bucket 
+    if st.button('Submit'):
+        with st.spinner('Retrieving details for the file you selected, wait for it....!'):
+            time.sleep(5)
+            if check_file_exists(name_of_file, USER_BUCKET_NAME):
+                st.write(f"The file {name_of_file} already exists in the {USER_BUCKET_NAME} bucket.")
+            else:
+                st.write(f"The file {name_of_file} does not exist in the {USER_BUCKET_NAME} bucket.")
+                transfer_file_to_S3()
+
+
+with col2:
+
+    # # filename_entered = st.text_input("Enter filename for download", placeholder='OR_ABI-L1b-RadC-M6C01_G18_s20230212001171_e20230212003548_c20230212003594.nc')
+    # def generate_url_from_filename():
+    #     # Get the filename entered by the user
+    #     filename = st.text_input("Enter the filename:")
+    #     if filename:
+    #         if st.button("Go to website"):
+    #             with st.spinner('Fetching link to file... '):
+    #                 time.sleep(3)
+
+    #                 # Base URL of the Nexrad website
+    #                 base_url = "https://noaa-nexrad-level2.s3.amazonaws.com/index.html#"
+                    
+    #                 # Combine the base URL with the year, day of year, and hour to get the URL
+    #                 file_url = base_url + filename
+                    
+    #                 # Display the URL
+    #                 st.write("Access :", file_url)
+
+    # generate_url_from_filename()
+
+    # https://noaa-nexrad-level2.s3.amazonaws.com/index.html#2023/02/01/KABR/
+    # https://noaa-nexrad-level2.s3.amazonaws.com/index.html2023/02/01/KABR/
+    # KABR20230201_000458_V06
+
+    def generate_url_from_filename():
+        # Get the filename entered by the user
+        filename = st.text_input("Enter the filename:")
+        if filename:
+            if st.button("Go to website"):
+                with st.spinner('Fetching link to Nexrad bucket...'):
+                    time.sleep(3)
+                    # Split the filename into parts
+                    parts = filename.split("_")
+                    
+                    # Get the year, day of year, and hour from the filename
+                    # KABR20230201
+                    station_code = parts[0][:4]
+                    year = parts[0][4:8]
+                    day_of_year = parts[0][8:10]
+                    hour = parts[0][10:]
+                    
+                    # Base URL of the Nexrad website
+                    base_url = "https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{}/{}/{}/{}/".format(year,day_of_year,hour,station_code)
+                    
+                    # Combine the base URL with the year, day of year, and hour to get the URL
+                    # file_url = base_url + f"{year}/{day_of_year}/{hour}/{station_code}/"
+                    
+                    # Display the URL
+                    st.write("Access link:", base_url)
+
+    generate_url_from_filename()
